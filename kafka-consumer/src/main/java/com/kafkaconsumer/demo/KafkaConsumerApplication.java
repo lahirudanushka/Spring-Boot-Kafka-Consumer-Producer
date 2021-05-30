0cb0defc1da6cb3590950095630957308449fcf5
Lahiru Danushka <hirud94@gmail.com>
2021-05-30 23:21:34 +0530

    Initial

--- kafka-consumer/src/main/java/com/kafkaconsumer/demo/KafkaConsumerApplication.java
+++ kafka-consumer/src/main/java/com/kafkaconsumer/demo/KafkaConsumerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaConsumerApplication {
-
+//sample
     public static void main(String[] args) {
         SpringApplication.run(KafkaConsumerApplication.class, args);
     }
